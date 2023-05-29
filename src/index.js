fa089cfccc2ab96de2580c648c554cf7085a5a4b
alerileri <ale.ortmed@gmail.com>
2023-05-29 17:21:47 -0600

    package-lock.json agregado

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+let mensaje = document.getElementById(mensaje).value
+console.log(mensaje)
+
 document.getElementById("mensaje-encriptado").style.display = "none";
+
 document.getElementById("no-mensaje").style.display = "none";
