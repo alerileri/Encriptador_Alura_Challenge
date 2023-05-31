b141f1c36ed57a2a85c9fbb2f970274ebab47214
alerileri <ale.ortmed@gmail.com>
2023-05-30 18:46:10 -0600

    Agregadas funciones de encriptar y desencriptar en encriptar.js; agregada lógica en index.js para aplicar las funciones encriptar y desencriptar y mostrar los resultados en las pantallas

--- src/encriptar.js
+++ src/encriptar.js
@@ -1,9 +1,24 @@
-function encriptado (){
-    const letraE = valorMensaje.textContent.replace('e','enter');
-    const letraI = valorMensaje.textContent.replace('e','imes');
-    const letraA = valorMensaje.textContent.replace('e','ai');
-    const letraO = valorMensaje.textContent.replace('e','ober');
-    const letraU = valorMensaje.textContent.replace('e','ufat');
-
-
+export function encriptar (mensaje){
+    return mensaje
+    .replace(/e/g, 'enter')
+    .replace(/i/g, 'imes')
+    .replace(/a/g, 'ai')
+    .replace(/o/g, 'ober')
+    .replace(/u/g, 'ufat');
 }
+
+export function desencriptar (mensajeEncriptado) {
+    return mensajeEncriptado
+    .replace(/enter/g, 'e')
+    .replace(/imes/g, 'i')
+    .replace(/ai/g, 'a')
+    .replace(/ober/g, 'o')
+    .replace(/ufat/g, 'u');
+}
+
+// function mostrarMensajeEncriptado(stringMensajeEncriptado) {
+//     mensajeEncriptado.value = stringMensajeEncriptado;
+//     mensajeEncriptado.style.display='block';
+//     pantallaNoMensaje.style.display='none'
+// }
+
