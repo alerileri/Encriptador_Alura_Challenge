48bdd074d4de2a6718066476ba0e6708a0f7ec52
alerileri <ale.ortmed@gmail.com>
2023-08-11 01:12:41 -0600

    Add encriptar, desencriptar, btnEncriptar and btn Desencriptar functions

--- src/encriptar.js
+++ src/encriptar.js
@@ -1,20 +1,25 @@
-export function encriptar (mensaje){
-    return mensaje
-    .replace(/e/g, 'enter')
-    .replace(/i/g, 'imes')
-    .replace(/a/g, 'ai')
-    .replace(/o/g, 'ober')
-    .replace(/u/g, 'ufat');
+
+
+/*export function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
 }
 
-export function desencriptar (mensajeEncriptado) {
+/*export function desencriptar(mensajeEncriptado) {
     return mensajeEncriptado
-    .replace(/enter/g, 'e')
-    .replace(/imes/g, 'i')
-    .replace(/ai/g, 'a')
-    .replace(/ober/g, 'o')
-    .replace(/ufat/g, 'u');
-}
+        .replace(/enter/g, 'e')
+        .replace(/imes/g, 'i')
+        .replace(/ai/g, 'a')
+        .replace(/ober/g, 'o')
+        .replace(/ufat/g, 'u');
+}*/
 
 // function mostrarMensajeEncriptado(stringMensajeEncriptado) {
 //     mensajeEncriptado.value = stringMensajeEncriptado;
--- src/script.js
+++ src/script.js
@@ -1,6 +1,49 @@
-import { encriptar, desencriptar } from './encriptar.js';
 
-document.addEventListener('DOMContentLoaded', function () {
+const textArea = document.querySelector(".text-area");
+const mensaje = document.querySelector(".mensaje");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value = "";
+    mensaje.style.backgroundImage = "none"
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase()
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada
+}
+
+
+
+function btnDesencriptar() {
+    const textoEncriptado = desencriptar(textArea.value)
+    mensaje.value = textoEncriptado
+    textArea.value="";
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [["e", "enter"], ["i", "imes"], ["a", "ai"], ["o", "ober"], ["u", "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for (let i = 0; i < matrizCodigo.length; i++) {
+        if (stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada
+}
+
+
+
+/*document.addEventListener('DOMContentLoaded', function () {
     //const form = document.getElementById('form-mensaje');
     const btnEncriptar = document.getElementById('encriptar');
     const btnDesencriptar = document.getElementById('desencriptar');
@@ -31,4 +74,4 @@ document.addEventListener('DOMContentLoaded', function () {
             noMensaje.style.display = 'block';
         }
     });
-});
+});*/
