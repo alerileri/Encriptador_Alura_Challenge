a005a3dd7b6e5d895ada6f5298214390f16c9ec7
alerileri <ale.ortmed@gmail.com>
2023-08-11 13:40:53 -0600

    Change to clean code

--- src/script.js
+++ src/script.js
@@ -40,38 +40,3 @@ function desencriptar(stringDesencriptada){
     }
     return stringDesencriptada
 }
-
-
-
-/*document.addEventListener('DOMContentLoaded', function () {
-    //const form = document.getElementById('form-mensaje');
-    const btnEncriptar = document.getElementById('encriptar');
-    const btnDesencriptar = document.getElementById('desencriptar');
-    const mensajeInput = document.getElementById('mensaje');
-    const mensajeEncriptado = document.getElementById('mensaje-secreto');
-    const noMensaje = document.getElementById('no-mensaje');
-
-    btnEncriptar.addEventListener('click', function(event){
-        event.preventDefault();
-        const valorMensaje = mensajeInput.value.trim();
-        if (valorMensaje != '') {
-            const stringMensajeEncriptado = encriptar(valorMensaje);
-            mensajeEncriptado.textContent = stringMensajeEncriptado;
-            noMensaje.style.display = 'none'
-        } else {
-            mensajeEncriptado.style.display = 'none'
-            noMensaje.style.display = 'block';
-        }        
-    });
-
-    btnDesencriptar.addEventListener('click', function(event){
-        event.preventDefault();
-        const valorMensajeEncriptado = mensajeEncriptado.value.trim();
-        if(valorMensajeEncriptado !== '') {
-            const stringMensajeDesencriptado = desencriptar(valorMensajeEncriptado);
-            mensajeInput.value = stringMensajeDesencriptado;
-            mensajeEncriptado.style.display = 'none';
-            noMensaje.style.display = 'block';
-        }
-    });
-});*/
