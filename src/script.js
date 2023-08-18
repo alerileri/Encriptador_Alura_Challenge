8e41ffb277f48a54d244d46920b4ce3c8547b1e3
alerileri <ale.ortmed@gmail.com>
2023-08-17 19:32:29 -0600

    Fix copy button style

--- src/script.js
+++ src/script.js
@@ -1,14 +1,18 @@
 
 const textArea = document.querySelector(".text-area");
 const mensaje = document.querySelector(".mensaje");
+const ningunMensaje = document.querySelector(".ningun-mensaje");
+const ingresaTexto = document.querySelector(".ingresa-texto");
 
 function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value)
     const divCopiar = document.querySelector(".div-copiar");
 
-    mensaje.value = textoEncriptado
+    mensaje.value = textoEncriptado;
     textArea.value = "";
-    mensaje.style.backgroundImage = "none"
+    mensaje.style.backgroundImage = "none";
+    ningunMensaje.style.display = "none";
+    ingresaTexto.style.display = "none";
     divCopiar.style.display = 'block';
 }
 
